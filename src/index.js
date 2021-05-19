1b668486bc9222ded823f1ba2a279eec6380e157
tiffany-shecodes <84154174+tiffany-shecodes@users.noreply.github.com>
2021-05-19 18:29:53 +0100

    Added unit conversion for Celsius and Fahrenheit

--- src/index.js
+++ src/index.js
@@ -22,9 +22,10 @@ h2.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`
 
 function showTemperature(response) {
 document.querySelector("#city").innerHTML = response.data.name;
-document.querySelector("#temperature").innerHTML = Math.round(
-  response.data.main.temp);
+document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
   let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
 
 document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 document.querySelector("#wind").innerHTML = Math.round (response.data.wind.speed);
@@ -62,15 +63,29 @@ function getCurrentLocation(event){
 function convertToFahrenheit(event){
   event.preventDefault();
 let temperatureElement = document.querySelector("#temperature");
-let temperature = temperatureElement.innerHTML;
-temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+
+function convertToCelsius(event){
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
+let celsiusTemperature = null;
 
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
  
 
 let searchForm = document.querySelector("#search-form");
