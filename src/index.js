c22138f7546d6c1aeb619f27e10264f1f63712f8
tiffany-shecodes <84154174+tiffany-shecodes@users.noreply.github.com>
2021-05-27 15:54:31 +0100

    final updates

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ let iconElement = document.querySelector("#icon");
 document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 document.querySelector("#wind").innerHTML = Math.round (response.data.wind.speed);
 document.querySelector("#description").innerHTML = response.data.weather[0].description;
-iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) = ``;
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 
 getForecast(response.data.coord);
